5f6ff82b58316fbad53bc45232c3aa52398d13dd
Hirak Santra <58108784+hirak-santra@users.noreply.github.com>
2020-07-08 15:22:58 +0530

    Create webpack-custom.js

---
+++ webpack-custom.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const pr = require('os');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+
+
+const generateConfigJson = (configuration)=> {
+    let mode = 'none';
+    let minimize = false;
+    let stats = {
+        children: false
+    };
+    if(process.argv && process.argv.length && process.argv.includes('--prod')){
+        mode = 'production';
+        minimize = true;
+    }    
+    return [
+                {
+                    target: 'web',
+                    stats: stats,
+                    optimization: {
+                        minimize: minimize
+                    },
+                    entry: configuration.js.bundles,
+                    output: {
+                        path: path.join(__dirname, configuration.outputJsPath),
+                        filename: configuration.js.filename
+                    },
+                    mode: mode
+                },
+                {
+                    target: 'web',
+                    stats: stats,
+                    optimization: {
+                        minimize: minimize,
+                        minimizer: [new OptimizeCSSAssetsPlugin({})]
+                    },
+                    plugins: [
+                        new ExtractTextPlugin(configuration.css.filename),
+                    ],
+                    entry: configuration.css.bundles,
+                    output: {
+                        path: path.join(__dirname, configuration.outputCssPath),
+                        filename: configuration.css.filename
+                    },
+                    module: {
+                        rules: [
+                            {
+                                test: /\.css$/,
+                                use: ExtractTextPlugin.extract({
+                                        fallback: 'style-loader',
+                                        use: 'css-loader'
+                                    })
+                            },
+                            {
+                                test: /\.(woff|woff2|eot|ttf)$/,
+                                exclude: /node_modules/,
+                                loader: 'file-loader',
+                                options: {
+                                    name: '../[path][name].[ext]',
+                                    emitFile: false,
+                                }
+                            },
+                            {
+                                test: /\.(svg|png|jpe?g|gif)$/,
+                                exclude: /node_modules/,
+                                loader: 'file-loader',
+                                options: {
+                                    name: '../[path][name].[ext]',
+                                    emitFile: false,
+                                }
+                            },
+                        ]
+                    },
+                    mode: mode
+                }
+            ];
+};
+
+
+module.exports = {generateConfigJson}
